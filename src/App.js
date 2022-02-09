2a8432583c03bf8958ffe689f0a50dfe13228497
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-08 21:56:59 -0300

    reset css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import './styles/reset.css'
 import './styles/globalStyles.css'
 import { Home } from './pages/home/index'
 import { Footer } from './components/footer/index'
-
 
 function App() {
 
