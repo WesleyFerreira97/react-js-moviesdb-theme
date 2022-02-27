c2c3a7e0ce64af463e5c62cc80a0e1123bd40cc3
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-26 23:18:36 -0300

    Added global style with styled components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './styles/globalStyles.css'
 import { Home } from './pages/home/index'
 import { Footer } from './components/footer/index'
+import { GlobalStyle } from './styles/global.ts'
 
 function App() {
 
   return (
     <>
+      <GlobalStyle />
       <Home />
       <Footer />
     </>
